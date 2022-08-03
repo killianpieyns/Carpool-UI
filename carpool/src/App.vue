877004c48ca5330f2d24b13ef86db58c16f48aeb
killian.pieyns <killian.pieyns@student.odisee.be>
2022-08-03 20:59:39 +0200

    initial commit + map test

---
+++ carpool/src/App.vue
@@ -0,0 +1,38 @@
+<script>
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+export default {
+  name: "Map",
+  data() {
+    return {
+      center: [50.8534, 4.347]
+    }
+  },
+  methods: {
+    setupLeafletMap: function () {
+      const mapDiv = L.map("mapContainer").setView(this.center, 10);
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '© OpenStreetMap'
+      }).addTo(mapDiv);
+    },
+  },
+  mounted() {
+    this.setupLeafletMap();
+  },
+};
+</script>
+
+<template>
+  <div id="container">
+    <div id="mapContainer"></div>
+  </div>
+</template>
+
+<style scoped>
+#mapContainer {
+  width: 98vw;
+  height: 98vh;
+}
+</style>
